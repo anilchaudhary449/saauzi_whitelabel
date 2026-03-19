7ae60739994c0ba64f719b90841965e6ffe4ab91
ANILCHAUDHARY <anil.bhumal@gmail.com>
2026-03-19 14:27:42 +0545

    test_login for login

--- tests/saauzi_whitelabel_test.py
+++ tests/saauzi_whitelabel_test.py
@@ -1,22 +1,29 @@
 import pytest
+
+from pages.login import Login
 from pages.signup import SignUp
 from test_data.test_data import *
 
-class TestSignup:
-
+class TestSaauziWhitelabel:
+    email = random_email()
+    password = random_password()
     def test_signup(self, browserSetup):
-        email = random_email()
-        password = random_password()
+
         company_name = random_company_name()
         company_owner = random_company_owner()
 
         signup_page = SignUp(browserSetup)
-        signup_page.enter_email(email)
-        signup_page.enter_password(password)
+        signup_page.enter_email(self.email)
+        signup_page.enter_password(self.password)
         signup_page.enter_company_name(company_name)
         signup_page.enter_company_owner(company_owner)
         signup_page.click_submit()
 
+    def test_login(self, browserSetup):
+        login_page = Login(browserSetup)
+        login_page.enter_email(self.email)
+        login_page.enter_password(self.password)
+        login_page.click_submit()
 
 
 if __name__ == "__main__":
