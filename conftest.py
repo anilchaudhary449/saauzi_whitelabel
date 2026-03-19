d6ad6c468bbeeefb9872b9512bd469209abbe7b5
ANILCHAUDHARY <anil.bhumal@gmail.com>
2026-03-19 14:13:50 +0545

    chore:url for register

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 @pytest.fixture(scope="session")
 def browserSetup():
     driver = webdriver.Chrome()
-    driver.get("https://partner.saauzi.com/")
+    driver.get("https://partner.saauzi.com/register")
     driver.maximize_window()
     yield driver
     driver.quit()
