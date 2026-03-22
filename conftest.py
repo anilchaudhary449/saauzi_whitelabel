2e9d00980a847e332a9a9950835e3166a873cfe1
ANILCHAUDHARY <anil.bhumal@gmail.com>
2026-03-22 17:11:10 +0545

    url updated

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 @pytest.fixture(scope="session")
 def browserSetup():
     driver = webdriver.Chrome()
-    driver.get("https://partner.saauzi.com/register")
+    driver.get("https://partner.saauzi.com/")
     driver.maximize_window()
     yield driver
     driver.quit()
