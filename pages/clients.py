2bf30af9cd482d0eff562e1da975757872c8760c
ANILCHAUDHARY <anil.bhumal@gmail.com>
2026-03-24 12:00:35 +0545

    feat: client view

--- pages/clients.py
+++ pages/clients.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,6 +9,7 @@ class Clients:
         self.ClientsTab = (By.XPATH,"//a[@href='/clients' and contains(text(),'Clients')]")
         self.CreateClientButton = (By.XPATH,"//div[contains(@class,'items-center') and normalize-space(.)='Create New Client']")
         self.CreateClient = (By.XPATH,"//div[@class='flex items-center gap-x-2' and contains(normalize-space(.),'Create Client')]")
+        self.actionView = (By.XPATH,"//button[@title='View Client']")
 
     def create_client(self):
         clients_tab = self.wait.until(EC.element_to_be_clickable(self.ClientsTab))
@@ -15,9 +17,10 @@ class Clients:
         assert clients_tab.is_enabled(), "Clients tab is not enabled in the navigation bar"
         clients_tab.click()
 
-        create_client_button = self.wait.until(EC.element_to_be_clickable(self.CreateClientButton))
+        create_client_button = self.wait.until(EC.presence_of_element_located(self.CreateClientButton))
         assert create_client_button.is_displayed(), "Create Client button is not visible on the Clients page"
         assert create_client_button.is_enabled(), "Create Client button is not enabled on the Clients page"
+        time.sleep(2)
         create_client_button.click()
         self.wait.until(EC.url_contains("/clients/create"))
         assert "/clients/create" in self.driver.current_url, "Clicking Create Client did not navigate to the client creation page"
@@ -31,9 +34,18 @@ class Clients:
         client_phone_field.send_keys(client_phone)
 
     def submit_form(self):
-        create_client_button = self.wait.until(EC.element_to_be_clickable(self.CreateClient))
+        create_client_button = self.wait.until(EC.presence_of_element_located(self.CreateClient))
         assert create_client_button.is_displayed(), "Create Client button is not visible on the client creation page"
         assert create_client_button.is_enabled(), "Create Client button is not enabled on the client creation page"
         create_client_button.click()
-        self.wait.until(EC.url_contains("/clients"))
+        WebDriverWait(self.driver, 60).until(EC.url_contains("/clients"))
         assert "/clients" in self.driver.current_url, "Submitting the client creation form did not navigate back to the clients page"
+
+    def view_client(self):
+        view_button = self.wait.until(EC.presence_of_element_located(self.actionView))
+        assert view_button.is_displayed(), "View Client button is not visible in the clients list"
+        assert view_button.is_enabled(), "View Client button is not enabled in the clients list"
+        time.sleep(0.5)
+        view_button.click()
+        WebDriverWait(self.driver, 60).until(EC.invisibility_of_element("/clients"))
+        assert "/details" in self.driver.current_url, "Clicking View Client did not navigate to the client details page"
