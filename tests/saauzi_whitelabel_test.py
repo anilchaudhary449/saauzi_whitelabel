55de5929876981189d04cd6e0a7f7234fce04f92
ANILCHAUDHARY <anil.bhumal@gmail.com>
2026-03-24 10:43:55 +0545

    test client included for test.

--- tests/saauzi_whitelabel_test.py
+++ tests/saauzi_whitelabel_test.py
@@ -1,7 +1,6 @@
-import time
-
 import pytest
-
+from pages.clients import Clients
+from pages.dashboard import Dashboard
 from pages.login import Login
 from pages.signup import SignUp
 from pages.home import Home
@@ -39,6 +38,16 @@ class TestSaauziWhitelabel:
         login_page.enter_email(self.email)
         login_page.enter_password(self.password)
         login_page.click_submit()
+    def test_dashboard(self, browserSetup):
+        dashboard_page = Dashboard(browserSetup)
+        dashboard_page.dashboard()
+        
+    def test_client(self, browserSetup):
+        client_name, client_email, client_phone = client_data()
+        client = Clients(browserSetup)
+        client.create_client()
+        client.fill_form(client_name, client_email, client_phone)
+        client.submit_form()
 
 
 if __name__ == "__main__":
