22a4fbfb1e652a8e3dab4003e3b88238a89bdddd
ANILCHAUDHARY <anil.bhumal@gmail.com>
2026-03-24 12:06:00 +0545

    feat: options included

--- conftest.py
+++ conftest.py
@@ -1,9 +1,18 @@
 import pytest
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="session")
 def browserSetup():
-    driver = webdriver.Chrome()
+    options = Options()
+    if os.getenv('CI'):
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+    
+    driver = webdriver.Chrome(options=options)
     driver.get("https://partner.saauzi.com/")
     driver.maximize_window()
     yield driver
